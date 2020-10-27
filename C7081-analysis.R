db0546996c8fb993ccd02b29df9d931b6246d862
Florence Galliers <florence_galliers@hotmail.com>
2020-10-27 11:24:51 +0000

    Start some regression tasks

--- C7081-analysis.R
+++ C7081-analysis.R
@@ -3,21 +3,92 @@
 ## Testing Data for Assessment 
 ####
 
+# read in data file
+setwd("~/Google Drive/Harper/1 Statistical Analysis for Data Science/Asssesment/github-C7081/C7081-assessment")
 library(openxlsx)
-
 data <- read.xlsx("housing_data_assessment.xlsx")
 
-dim(data)
+# EDA
 
-summary(data)
+# look if any values are = 0, assign them to variable
+zero_values <- which(data$price == 0)
+# remove these from the data set as house price cannot = 0
+data <- data[-zero_values,]
+# we are now left with 4551 observations
 
-data.lm <- lm(data$price ~ . -city-basement-yr_built-renovated, 
-   data = data)
+# plot price against sqft_living
+plot(data$price, data$sqft_living,
+     xlab = "House Price ($)",
+     ylab = "Size of House (sqft)",
+     main = "House Price - House Size")
+# we can see there are some outliers, let's calculate IQR to have some actual 
+# values to remove outliers, rather than doing it by eye
 
-summary(data.lm)
+# start with price
+summary(data$price)
+# 1st Q = 326264
+# 3rd Q = 657500
+657500 - 326264
+# IQR = 3rdQ - 1stQ = 331236
+331236*3 # = 993708
+# subtracting IQR*3 value from the 1stQ does not leave any outstanding values
+# adding IQR*3 value to the 3rdQ 
+657500+993708 # = 1651208
+# anything above $1651208 will be counted as an outlier
+which(data$price > 1651208)
+# this gives 89 values, lets remove them from the dataset and re-plot the data
+outliers_price <- which(data$price > 1651208)
+data <- data[-outliers_price, ]
+# leaves us with 4462 observations
+# plot again
+plot(data$price, data$sqft_living,
+     xlab = "House Price ($)",
+     ylab = "Size of House (sqft)",
+     main = "House Price - House Size")
 
-summary(data$city)
+# lets do the same with sqft_living
+summary(data$sqft_living)
+# 1st Q = 1450
+# 3rd Q = 2568
+2568 - 1450
+# IQR = 3rdQ - 1stQ = 1118
+1118*3 # = 3354
+# subtracting IQR*3 value from the 1stQ does not leave any outstanding values
+# adding IQR*3 value to the 3rdQ 
+2568+3354 # = 5922
+# anything above 5922 counted as an outlier
+outliers_sqft_living <- which(data$sqft_living > 5922) # 8 values
+# remove outliers from data set
+data <- data[-outliers_sqft_living, ]
+# left with 4454 observations
 
-plot(data$sqft_living, data$price)
+# plot again
+plot(data$price, data$sqft_living,
+     xlab = "House Price ($)",
+     ylab = "Size of House (sqft)",
+     main = "House Price - House Size")
 
-plot(data$price)
+# create histogram of number of bedrooms
+hist(data$bed,
+     main = "Histogram: Number of Bedrooms",
+     xlab = "Number of Bedrooms",
+     ylab = "Frequency")
+# we can see number of bedrooms is discrete values, normally distributed
+
+# create histogram of number of bathrooms
+hist(data$bath,
+     main = "Histogram: Number of Bathrooms",
+     xlab = "Number of Bathrooms",
+     ylab = "Frequency")
+
+
+
+
+
+
+
+data_lm <- lm(data$price ~ . -city-basement-yr_built-renovated, 
+              data = data)
+summary(data_lm)
+
+plot(data_lm)
---
+++ C7081-regression.R
@@ -0,0 +1,253 @@
+## HEADER ####
+## 2020-10-27
+## C7081-Assessment
+
+# Set WD, Read in Data
+setwd("~/Google Drive/Harper/1 Statistical Analysis for Data Science/Asssesment/github-C7081/C7081-assessment")
+library(openxlsx)
+data2 <- read.xlsx("housing_data_assessment.xlsx")
+
+# Split into training and test data sets
+
+data2 <- data2[ ,-11]
+
+set.seed(1)
+
+train <- sample(c(TRUE, FALSE), nrow(data2), rep = TRUE)
+
+test <- (!train)
+
+house.train <- data2[train,]
+
+house.test <- data2[test,]
+
+# Fit linear model using least squares on train data, report test error
+
+lm.model <- lm(price ~ ., 
+               data = house.train)
+
+lm.pred <- predict(lm.model, house.test)
+
+mean((house.test[, "price"] - lm.pred)^2)
+
+# Fit a ridge regression, choose lambda by cross validation
+
+library(glmnet)
+
+set.seed(1)
+
+x <- model.matrix(price ~ ., data = data2)[,-1]
+
+y <- data2$price
+
+cv.ridge <- cv.glmnet(x[train,], y[train], alpha = 0)
+
+plot(cv.ridge)
+
+best.lambda <- cv.ridge$lambda.min
+
+best.lambda
+
+# Report test error
+
+ridge.mod <- glmnet(x[train, ], y[train], alpha = 0)
+
+ridge.pred <- predict(ridge.mod, s = best.lambda,
+                      newx = x[test,])
+
+mean((ridge.pred - y[test])^2)
+
+# Fit a lasso model on the training set, choose lambda by C-V
+
+lasso.mod <- glmnet(x[train,], 
+                    y[train], 
+                    alpha = 1)
+plot(lasso.mod)
+
+set.seed(1)
+
+cv.lasso <- cv.glmnet(x[train,], 
+                      y[train],
+                      alpha = 1)
+
+plot(cv.lasso)
+
+best.lamb <- cv.lasso$lambda.min
+
+lasso.pred <- predict(lasso.mod, s = best.lamb, 
+                      newx = x[test,])
+
+# Report test error and number of non zero coefficients
+
+mean((lasso.pred - y[test])^2)
+
+out <- glmnet(x, y, alpha = 1)
+
+lasso.coef <- predict(out, type = "coefficients", 
+                      s = best.lamb)
+lasso.coef
+
+lasso.coef[lasso.coef!=0]
+
+# Fit PCR Model on training data, M chosen by C-V
+
+library(pls)
+
+set.seed(2)
+
+pcr.fit <- pcr(price ~ ., 
+               data = house.train,
+               scale = TRUE,
+               validation = "CV")
+
+summary(pcr.fit)
+
+validationplot(pcr.fit, val.type = "MSEP") 
+# shows the smallest C-V error is when M = 9 which is the same as just 
+# performing least squares
+
+names(pcr.fit)
+
+
+# Report test error, and value of M selected
+
+pcr.pred <- predict(pcr.fit, x[test,], ncomp = 9)
+
+mean((pcr.pred - y[test])^2)
+
+# Fit PLS Model to training data, C-V
+
+pls.fit <- plsr(price ~ .,
+                data = house.train,
+                scale = TRUE,
+                validation = "CV")
+
+summary(pls.fit)
+
+validationplot(pls.fit, val.type = "MSEP")
+
+# Report test error, and value of M selected
+
+pls.pred <- predict(pls.fit, x[test,], ncomp = 3)
+
+mean((pls.pred - y[test])^2)
+
+pls.fit <- plsr(price ~ ., data = data2, scale = TRUE, ncomp = 3)
+
+summary(pls.fit)
+
+# Best subset selection
+
+library(leaps)
+
+regsub.fit <- regsubsets(price ~ ., 
+                         data = data2[train, ],
+                         nvmax = 9)
+
+test.matrix <- model.matrix(price ~ .,
+                            data = data2[test,])
+
+val.errors <- rep(NA, 9)
+for(i in 1:9) {
+  coefi <- coef(regsub.fit, id = i)
+  pred <- test.matrix[ ,names(coefi)]%*%coefi
+  val.errors[i]=mean((data2$price[test]-pred)^2)
+}
+
+val.errors
+
+which.min(val.errors)
+
+coef(regsub.fit, 7)
+
+# create prediction formula 
+
+predict.regsubsets <- function(object, newdata, id, ...){
+  form <- as.formula(object$call[[2]])
+  mat <- model.matrix(form, newdata)
+  coefi <- coef(object, id=id)
+  xvars <- names(coefi)
+  mat[, xvars]%*%coefi
+}
+
+# Make use of full data set to obtain more accurate coef estimates
+
+regfit.best <- regsubsets(price ~ ., 
+                          data = data2, 
+                          nvmax = 9)
+
+coef(regfit.best, 7)
+
+reg.summary <- summary(regfit.best)
+
+
+# Choosing among models of different sizes using cross-validation
+
+k <- 10
+set.seed(1)
+folds <- sample(1:k, nrow(data2), replace = TRUE)
+cv.errors <- matrix(NA, k, 9, dimnames = list(NULL, paste(1:9)))
+
+for(j in 1:k){
+  best.fit <- regsubsets(price ~ ., 
+                         data = data2[folds!=j,],
+                         nvmax = 9)
+  for (i in 1:9) {
+    pred <- predict(best.fit, data2[folds==j,], id=i)
+    cv.errors[j, i]=mean((data2$price[folds==j]-pred)^2)
+  }
+}
+
+mean.cv.errors <- apply(cv.errors, 2, mean)
+mean.cv.errors
+
+plot(mean.cv.errors, type = "b")
+
+# Cross validation also selects a seven variable model
+
+# Plot graphs of RSS, adjr2, cp and BIC
+
+which.max(reg.summary$adjr2)
+
+which.min(reg.summary$cp)
+
+which.min(reg.summary$bic)
+
+
+par(mfrow=c(2,2))
+
+plot(reg.summary$rss, 
+     xlab="Number of Variables", 
+     ylab="RSS",
+     type = "l")
+
+plot(reg.summary$adjr2,
+     xlab = "Number of Variables",
+     ylab = "Adjusted RSq",
+     type = "l")
+
+points(7, reg.summary$adjr2[7], col="red", cex=2, pch=20)
+
+plot(reg.summary$cp, 
+     xlab = "Number of Variables",
+     ylab = "Cp",
+     type = "l")
+
+points(7, reg.summary$cp[7], # this is giving the coordinates of the point 
+       # on the graph to plot, 10 on the x axis and the 10th value of the cp on
+       # y axis
+       col = "red",
+       cex = 2,
+       pch = 20)
+
+plot(reg.summary$bic, 
+     xlab = "Number of Variables",
+     ylab = "BIC",
+     type = "l")
+
+points(5, reg.summary$bic[5],
+       col = "red",
+       cex = 2,
+       pch = 20)
+
+# Forward and Backward Stepwise Selection
