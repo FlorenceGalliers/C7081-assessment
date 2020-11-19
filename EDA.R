81777bf0ad2ee7273aafd58a46c43f2f9fef8bf7
Florence Galliers <florence_galliers@hotmail.com>
2020-11-19 12:55:22 +0000

    Report and EDA

--- EDA.R
+++ EDA.R
@@ -2,63 +2,62 @@
 ## 2020-10-27
 ## C7081-Exploratory Data Analysis
 
-#set working directory
+# Set the working directory
 setwd("~/Google Drive/Harper/1-C7081/Asssesment/github-C7081/C7081-assessment")
 
+# Load in required libraries
 library(plyr)
 library(writexl)
 library(psych)
 library(ggplot2)
 library(ggcorrplot)
+library(xlsx)
 
-
-#import data
+# Import original data set as downloaded from Kaggle
+# https://www.kaggle.com/shree1992/housedata
 data <- read.csv(file = "original-data.csv")
 
-#this script is intended to get to know the data, select the best variables to take
-#forward to my analysis and visualise any existing patterns or correlations in 
-#the data.
+# This script is intended to get to know the data, select the best variables to 
+# take forward in a cleaned data set to use in my analysis and visualise any
+# existing patterns or correlations in the data.
 
-#first I am going to go through each of the variables from the original data set
+# First I am will go through each of the variables from the original data set
 
-#1 date (not included in analysis)
+#1 Date (not included in analysis)
 summary(data$date)
-#this is a character variable giving the date of the sale of the house, as I will
-#not be looking at price over time, this variable is not going to be included in my
-#analysis.
+# This is a character variable giving the date of the sale of the house, as I will
+# not be looking at price over time, this variable is not going to be included in my
+# analysis.
 
-#2 price (dependent variable for analysis)
+#2 Price (dependent variable for analysis)
 summary(data$price)
-#numeric variable giving sale price of house in US dollars
-#we can see the max price is well above the 3rd quartile value, this may be due 
-#to some outlying values. Will look into this later.
-#best to convert this variable into thousands of dollars
+# Numeric variable giving sale price of house in US dollars
+# We can see the max price is well above the 3rd quartile value, this may be due 
+# to some outlying values. Will look into this later.
+# best to convert this variable into thousands of dollars to make it more manageable
 data$price <- data$price/1000
-# look if any values are = 0, assign them to variable
+# Look if any values are = 0, assign them to variable
 zero_values <- which(data$price == 0)
-# remove these from the data set as house price cannot = 0
+# Remove these from the data set as house price cannot be $0
 data <- data[-zero_values,]
-# we are now left with 4551 observations
 
-
-#3 bedrooms
+#3 Bedrooms
 summary(data$bedrooms)
-#between 0 and 9 bedrooms in each house, mean of just over 3 bedrooms
+# There are between 0 and 9 bedrooms in each house, mean of just over 3 bedrooms
 hist(data$bedrooms)
-#histogram shows normal distribution of number of bedrooms, can also see they 
-#are discrete values.
-plot(data$bedrooms) #normal distribution
-plot(data$bedrooms, data$price) #price increase with increase in bedrooms?
+# Histogram shows normal distribution of number of bedrooms, can also see they 
+# are discrete values.
+plot(data$bedrooms) # normal distribution
+plot(data$bedrooms, data$price) # price increase with increase in bedrooms?
 
-#4 bathrooms
+#4 Bathrooms
 summary(data$bathrooms)
-#between 0 and 8 bathrooms in each house, mean of just over 2 bathrooms
+#There are between 0 and 8 bathrooms in each house, mean of just over 2 bathrooms
 hist(data$bathrooms)
-plot(data$bathrooms, data$price) #may be positive correlation between no of 
-#bathrooms and price of house
+plot(data$bathrooms, data$price) # may be positive correlation between no of 
+# bathrooms and price of house
 bathrooms <- as.factor(data$bathrooms)
 plot(bathrooms, data$price)
-  
 
 #5 sqft_living
 summary(data$sqft_living) 
@@ -69,17 +68,17 @@ plot(data$sqft_living, data$price)
 summary(data$sqft_lot)
 plot(data$sqft_lot, data$price)
 
-#7 floors
+#7 Floors
 summary(data$floors) #between 1 and 3.5 floors for each house, mean of 1.5
 plot(data$floors, data$price)
 
-#8 waterfront (not included in analysis)
+#8 Waterfront (not included in analysis)
 plot(data$waterfront) #not many values for this
 data$waterfront <- as.factor(data$waterfront)
 count(data$waterfront) #only 33 values as 'YES' for having a waterfront
 #lets remove this variable from our dataset
 
-#9 view (not included in analysis)
+#9 View (not included in analysis)
 summary(data$view)
 plot(data$view)
 data$view <- as.factor(data$view)
@@ -87,7 +86,7 @@ count(data$view)
 #most houses score 0 for view, or the data is missing
 #lets remove this variable from our dataset.
 
-#10 condition
+#10 Condition
 summary(data$condition) #looks like 5 levelled rating on condition
 plot(data$condition)
 #lets convert to a factor
@@ -143,12 +142,12 @@ count(data$if_renovated)
 boxplot(data$if_renovated, data$price)
 data$if_renovated <- as.factor(data$if_renovated)
 
-#15 street (not included in analysis)
+#15 Street (not included in analysis)
 summary(data$street) #character variable giving street name
 data$street <- as.factor(data$street) #all unique names - not going to be much
 #help in predicting price so lets remove this from the dataset
 
-#16 city
+#16 City
 summary(data$city) #character variable giving city that houses are located in
 data$city <- as.factor(data$city)
 count(data$city) #gives factor with 44 levels, some only have one or two houses in
@@ -166,33 +165,52 @@ data <- data[-lowfreq_cities,] #left with 4532 observations after removing these
 data$city <- droplevels(data$city) #drop these unused levels from dataset
 plot(data$city, data$price) #definitely looks to be some cities with higher prices
           
-#17 statezip
+#17 State zipcode
 summary(data$statezip)
 data$statezip <- as.factor(data$statezip) #73 unique variables? is this too many?
 plot(data$statezip, data$price) #definitely some here with higher values than others
 
-plot(data$sqft_living, data$price)
-
-
-
-#18 country (not included in analysis)
-#constant variable, lets remove
+#18 Country (not included in analysis)
+# constant variable, so lets remove
 
 variables_to_remove <- c(1, 8, 9, 11, 15, 18)
+# Date, Waterfront, View, sqft_above, Street, Country
 data <- data[ ,-variables_to_remove]
 
-#we are left with 12 variables after the initial EDA and sorting out.
+# We are left with 12 variables after the initial EDA
 
-# remove outliers
+# Remove outliers identified throughout.
 outliers <- c(100, 121, 122, 2271, 2387, 2921, 4324, 4325, 4328, 4329)
 data <- data[-outliers, ]
 
+# Final number of observations is 4522
 
-#final number of observations is 4524
 
-#investigating correlations between price and other variables 
-pairs.panels(data, col="red")
+# Export the cleaned data set to carry forward for use in the analysis
 
+# Create a data dictionary
+data_descriptions <- c("House sale price in thousands of US dollars", 
+                       "Number of bedrooms",
+                       "Number of bathrooms",
+                       "Area of house in square feet",
+                       "Area of whole housing lot in square feet",
+                       "Number of floors in the house",
+                       "Condition of house, 1 to 5",
+                       "1 = if house has a basement, 0 = no basement",
+                       "Year that the house was built subtracted from 2014",
+                       "1 = if house has been renovated, 0 = if no renovation",
+                       "Location of house to the nearest city in Washington, USA",
+                       "Zip code of house")
+
+data_dictionary <- data.frame(names(data), data_descriptions)
+
+write.xlsx(data_dictionary, 
+            file = "cleaned-data.xlsx",
+            sheetName = "DICTIONARY",
+            append = TRUE)
+
+# Next I am going to look into the correlations between price and the other 
+# variables.
 corr_variables <- data.frame(data$price, data$bedrooms, data$bathrooms, 
                              data$sqft_living, data$sqft_lot,
                              data$floors, data$condition, data$house_age)
@@ -206,55 +224,10 @@ ggcorrplot(cor_mat,
            type = "lower",
            p.mat = p_mat,
            lab = T, lab_size = 2.5)
-# from this correlation we can see that the house price is most strongly 
+# From this correlation we can see that the house price is most strongly 
 # correlated with sqft_living, no of bathrooms, and then no of 
 # bedrooms and no of floors. These will be the variables we will focus on in
 # in our analysis.
-# the correlation between price and house_age is not significant. 
+# The correlation between price and house_age is not significant. 
 
 
-
-#split into training and testing sets
-set.seed(2) 
-n = nrow(data) #number of rows
-train_index = sample(1:n, size = round(0.8*n), replace=FALSE) 
-train = data[train_index ,] #takes 80% of the data for training set
-test = data[-train_index ,] #remaining 20% for the test set
-
-#least squares linear regression
-lm_model <- lm(price ~ ., 
-               data = train)
-# make predictions on test set
-lm_pred <- predict(lm_model, test) 
-# calculate MSE
-mean((test[, "price"] - lm_pred)^2)
-#28053.99
-summary(lm_model)
-
-# Diagnostic plots of the linear regression model
-par(mfrow=c(2,2))
-plot(lm_model)
-
-
-data2 <- data[ ,-12 ]
-data2 <- data2[ ,-11]
-
-#split into training and testing sets
-set.seed(2) 
-n = nrow(data2) #number of rows
-train_index = sample(1:n, size = round(0.8*n), replace=FALSE) 
-train2 = data2[train_index ,] #takes 80% of the data for training set
-test2 = data2[-train_index ,] #remaining 20% for the test set
-
-#least squares linear regression
-lm_model2 <- lm(price ~ ., 
-               data = train2)
-# make predictions on test set
-lm_pred2 <- predict(lm_model2, test2) 
-# calculate MSE
-mean((test[, "price"] - lm_pred)^2)
-summary(lm_model2)
-
-# Diagnostic plots of the linear regression model
-par(mfrow=c(2,2))
-plot(lm_model2)
